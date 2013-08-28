9397d2b12895f46d14a1fed8dbd843e922f290f3
Robbe Van der Gucht <robbe.vandergucht@gmail.com>
2013-08-28 17:51:53 +0200

    mobile listview reqests over network

---
+++ www/js/debug.js
@@ -0,0 +1,6 @@
+var _DEBUG = true;
+var DEBUG = function(print){
+    if (_DEBUG) {
+        console.log(print);
+    }
+}
---
+++ www/js/server.js
@@ -0,0 +1,14 @@
+var server = {       
+    sendJSON : function (url, data, callback){
+        var request = new XMLHttpRequest();
+        request.open("POST", url);
+        request.onreadystatechange = function(){
+            if (request.readyState === 4 && callback != undefined) {
+                DEBUG(request);
+                callback(request);
+            }
+        };
+        request.setRequestHeader("Content-Type", "application/json");
+        request.send(JSON.stringify(data));
+    }
+};
